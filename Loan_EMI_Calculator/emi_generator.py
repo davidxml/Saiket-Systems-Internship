36697bae5a4c7c0ad9ea6e662907bb670864193e
davidxml <pycodegenius@gmail.com>
2025-10-04 18:17:16 +0100

    initial commit

---
+++ Loan_EMI_Calculator/emi_generator.py
@@ -0,0 +1,37 @@
+"""
+* This Module calculates and Returns the Equated Monthly Installments to 
+be paid by loan takers
+"""
+def calculate_emi(P, R, N):
+    """
+    This Calculates the Equated Monthly Installents given the:
+    - Principal
+    - Rate
+    - Number of Months (loan tenure)
+    """
+    emi_result = (P *R *((1 + R)**N))/(((1+R)**N)-1)
+    return emi_result
+
+def emi_engine():
+    """
+    Collects and processes user's input for:
+    - Principal
+    - Interest rate
+    - Loan tenure
+    """
+    P = float(input("Enter the value for Principal: #"))
+
+    r = float(input("Enter the value for Interest Rate: "))
+    is_annual = input("Is the rate annual? (yes or no): ")
+    if is_annual == "yes".lower():
+        R = r / 12/ 100
+    else:
+        R = r / 100
+    N = int(input("Enter the Loan tenure in months: "))
+    total_emi = calculate_emi(P, R, N)
+    print(f"The Equated Monthly Installment is {total_emi:,.2f}")
+    return total_emi
+
+
+if __name__ == "__main__":
+    emi_engine()
---
+++ Loan_EMI_Calculator/main.py
@@ -0,0 +1,11 @@
+from emi_generator import emi_engine        # This makes emi_engine available to be used.
+
+
+print("WElCOME TO MY LOAN EMI CALCULATOR \nLet's get to work!")
+print("Enter the data carefully and accurately, we don't want errors")
+
+while True:
+    emi_engine()
+    run_again = input("Do you want to Calculate Another EMI? (yes or no): ")
+    if run_again == "no".lower():
+        break
