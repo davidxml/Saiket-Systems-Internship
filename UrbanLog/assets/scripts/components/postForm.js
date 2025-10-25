ca5d95133292ed423b7d68de73a155322b9cd094
davidxml <pycodegenius@gmail.com>
2025-10-25 21:46:21 +0100

    fix

--- UrbanLog/assets/scripts/components/postForm.js
+++ UrbanLog/assets/scripts/components/postForm.js
@@ -2,10 +2,11 @@
 
 const postForm = document.getElementById('post-form');
 
-/**
+/*****
  * Sets up the event listener for the post creation form.
  * @param {function} createPostHandler - The function from app.js to call when the form is submitted.
- */
+ *
+*****/
 export const initForm = (createPostHandler) => {
     postForm.addEventListener('submit', (event) => {
         event.preventDefault(); // Stop the browser from performing a standard form submission
