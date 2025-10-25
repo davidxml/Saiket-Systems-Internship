1a6f45efbb6689981b9543c1bfc94c93e81f0dd3
davidxml <pycodegenius@gmail.com>
2025-10-25 21:44:34 +0100

    Added the logic that manages the form submission.

---
+++ UrbanLog/assets/scripts/components/postForm.js
@@ -0,0 +1,14 @@
+// postForm.js - Manages the form submission event.
+
+const postForm = document.getElementById('post-form');
+
+/**
+ * Sets up the event listener for the post creation form.
+ * @param {function} createPostHandler - The function from app.js to call when the form is submitted.
+ */
+export const initForm = (createPostHandler) => {
+    postForm.addEventListener('submit', (event) => {
+        event.preventDefault(); // Stop the browser from performing a standard form submission
+        createPostHandler();    // Call the main logic function
+    });
+};
