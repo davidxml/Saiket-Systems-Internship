b026265d13dc49f254339126ef6bd84bcae9f162
davidxml <pycodegenius@gmail.com>
2025-10-29 09:57:34 +0100

    Added the game's engine

---
+++ Number Guessing Game/game/core.py
@@ -0,0 +1,21 @@
+import random 
+
+class GuessingGame:
+    """Holds the game's core logic."""
+    def __init__ (self, min_num, max_num):
+        self.min_num = min_num
+        self.max_num = max_num
+        self.secret_number = random.randint(self.min_num, self.max_num)
+        self.guess_count = 0
+        self.game_over = False
+
+    def check_guess(self, guess):
+        """Takes a guess, compares it, and returns a hint"""
+        self.guess_count += 1   # tracks the guess
+        if guess == self.secret_number:
+            self.game_over = True
+            return "Excellent, your guess was correct"
+        elif guess < self.secret_number:
+            return " Your guess is low"
+        else: 
+            return "Your guess is high"
